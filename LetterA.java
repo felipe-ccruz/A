c786899362f64f74584e21cb67abe49b34fa2493
Felipe Cruz <felipe23070305@aluno.cesupa.br>
2024-06-15 14:40:44 -0300

    instanciação de objetos

--- LetterA.java
+++ LetterA.java
@@ -1,4 +1,16 @@
 public class LetterA {
     private char maiuscula;
+
+    public LetterA(char maiuscula) {
+        this.maiuscula = maiuscula;
+    }
+
+
+    public char getMaiuscula() {
+        return maiuscula;
+    }
+    public void setMaiuscula(char maiuscula) {
+        this.maiuscula = maiuscula;
+    }
     
 }
--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public class app{
     public static void main(String[] args) {
-        
+        LetterA first = new LetterA('U');
+        LetterA second = new LetterA('L');
     }
 }
